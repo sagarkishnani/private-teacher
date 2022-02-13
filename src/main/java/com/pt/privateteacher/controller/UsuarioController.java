7b05b781a3b44a53cd656228a2cccc7df38c8b9d
Sagar <sagarkishnani67@gmail.com>
2022-02-12 23:08:36 -0500

    SPRINT 3 - Inscribirse a curso

--- src/main/java/com/pt/privateteacher/controller/UsuarioController.java
+++ src/main/java/com/pt/privateteacher/controller/UsuarioController.java
@@ -1,11 +1,16 @@
 package com.pt.privateteacher.controller;
 
+import com.pt.privateteacher.model.Inscripcion;
+import com.pt.privateteacher.model.Tutor;
 import com.pt.privateteacher.model.Usuario;
+import com.pt.privateteacher.repository.InscripcionRepository;
+import com.pt.privateteacher.repository.TutorRepository;
 import com.pt.privateteacher.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
@@ -17,9 +22,36 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    @Autowired
+    private InscripcionRepository inscripcionRepository;
+
+    //http://localhost:8080/usuario/12/inscribir
+    @GetMapping("/usuario/inscribir/{idtutor}")
+    String inscribir(@PathVariable Integer idtutor,
+                     Principal principal)
+    {
+            Usuario usuario = usuarioRepository.findByEmail(principal.getName()).get();
+            Tutor tutor = tutorRepository.getById(idtutor);
+
+            Inscripcion inscripcion = new Inscripcion();
+            inscripcion.setUsuario(usuario);
+            inscripcion.setTutor(tutor);
+            inscripcion.setFechaInscripcion(LocalDateTime.now());
+
+            inscripcionRepository.save(inscripcion);
+
+            return "redirect:/tutores";
+    }
+
+    @GetMapping("/mis-cursos")
     String misCursos(Principal principal, Model model)
     {
         Usuario usuario = usuarioRepository.findByEmail(principal.getName()).get();
+        List<Inscripcion> inscripciones = inscripcionRepository.findByUsuario(usuario);
+        model.addAttribute("inscripciones", inscripciones);
         return "mis-cursos";
     }
 }
